bfb72ea04979ca183c6856f97ca2da81d36399a6
Alan McGinlay <amcginlay@users.noreply.github.com>
2019-10-04 15:25:24 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,19 +2,23 @@ const greeting = process.env.GREETING || 'Hello';
 const port = process.env.PORT || 8080;
 const serverstarthr = process.hrtime();
 
-require('http').createServer(function (req, res) {
-  // if workload parameter provided then simulate workload
+function doWork(workload) {
   const reqstarthr = process.hrtime();
-  const workload = parseFloat(require('url').parse(req.url, true).query.workload) || 0.0;
   while (true) {
     const elapsedhr = process.hrtime(reqstarthr);
-    const elapsed = ((elapsedhr[0] * 1e9) + elapsedhr[1]) / 1e6; // hr -> msecs
+    const elapsed = ((elapsedhr[0] * 1e9) + elapsedhr[1]) / 1e6; // convert hr to msecs
     if (elapsed > workload) break;
   }
-  // respond with greeting and copy to stdout
+}
+
+function requestHandler(req, res) {
+  const workload = parseFloat(require('url').parse(req.url, true).query.workload) || 0.0;
+  doWork(workload)
   var result = greeting + ' from ' + require('os').hostname() + ' at ' + process.hrtime(serverstarthr);
   console.log('> ' + result);
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end(result + '\n');
-}).listen(port);
+}
+
+require('http').createServer(requestHandler).listen(port);
 console.log('Server running on ' + port);
